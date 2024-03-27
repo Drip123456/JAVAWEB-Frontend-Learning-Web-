a97cde358067f4f4e075c78a99cfd55bce7a57ad
Drip123456 <3162304527@qq.com>
2024-03-27 22:59:51 +0800

    更新vue的父子传参

---
+++ Z-vue04/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, reactive } from 'vue';
+// 引入多个 .vue组件，之后打包到一个vue文件，挂载到div上
+import Header from './components/Header.vue'
+import Navigator from "./components/Navigator.vue";
+import Content from "./components/Content.vue";
+
+let msg = ref('')
+function receiver(data) {
+  msg.value = data
+}
+</script>
+
+<template>
+  {{ msg }}
+  <hr>
+  <Header class="header"></Header>
+  <Navigator @send-menu="receiver" class="navigator"></Navigator>
+  <Content :message="msg" class="content"></Content>
+</template>
+
+<style scoped>
+.header {
+  border: 1px solid red;
+  height: 80px;
+}
+
+.navigator {
+  border: 1px solid green;
+  width: 20%;
+  height: 500px;
+  float: left;
+}
+
+.content {
+  border: 1px solid blue;
+  width: 79%;
+  height: 500px;
+  float: right;
+}
+</style>
---
+++ Z-vue04/src/components/Content.vue
@@ -0,0 +1,18 @@
+<script setup>
+// 接受父组件
+import { ref, reactive, defineProps } from 'vue';
+defineProps({
+    // 默认响应式
+    message: String
+})
+
+</script>
+
+<template>
+    <div>
+        <!-- 这里是主要展示主要内容 -->
+        {{ message }}
+    </div>
+</template>
+
+<style scoped></style>
